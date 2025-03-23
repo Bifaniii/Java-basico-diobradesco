d3b6cb2e88c26d750951c74a2de7f32a9bad0975
Bifaniii <guilherme.bifanis@gmail.com>
2025-03-23 17:25:26 -0300

    Primeira parte dos operadores

---
+++ operadores-aritmeticos/src/ClasseOperadores.java
@@ -0,0 +1,13 @@
+public class ClasseOperadores {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+/// Classe de Operadores.java
+/// String nome = "Guilherme";
+/// int idade = 21;
+/// double peso = 70.0;
+/// chat sexo = 'M';
+/// boolean doadorOrgao = false;
+/// Date dataNascimento = new Date();
